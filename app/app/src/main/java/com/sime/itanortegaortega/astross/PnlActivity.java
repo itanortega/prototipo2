fe5133fc0034d0d774b292b7a042183cc5c7d595
Christian Ortega <csortega@iucesmag.edu.co>
2018-05-22 18:00:59 -0500

    pnl

--- app/app/src/main/java/com/sime/itanortegaortega/astross/PnlActivity.java
+++ app/app/src/main/java/com/sime/itanortegaortega/astross/PnlActivity.java
@@ -8,6 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class PnlActivity extends AppCompatActivity {
     private static String LOCAL = "";
 
@@ -47,14 +51,26 @@ public class PnlActivity extends AppCompatActivity {
         Txt_frase2 = (TextView) this.findViewById(R.id.Txt_frase2);
         Txt_frase3 = (TextView) this.findViewById(R.id.Txt_frase3);
 
-        String f1 = Utilidades.getPNL(this, (int) (Math.random() * 4)+1);
-        String f2 = Utilidades.getPNL(this, (int) (Math.random() * 8)+5);
-        String f3 = Utilidades.getPNL(this, (int) (Math.random() * 12)+9);
+        CAFData data = CAFData.dataWithContentsOfFile(LOCAL + "/horoscopo.json");
+        if(data != null){
+            try {
+                JSONObject horoscoporoot = new JSONObject(data.toText());
+                JSONArray horoscopo = horoscoporoot.getJSONArray("horoscopo");
+                JSONObject datos_horoscopo = horoscopo.getJSONObject(id-1);
+                String numeros = datos_horoscopo.getString("pnl").toString();
+                String[] textos = numeros.split(";");
+                int i=1;
+                if(this.getResources().getString(R.string.prefijo_idioma).equals("en")){
+                    i = 0;
+                }
 
-        Txt_frase1.setText(f1);
-        Txt_frase2.setText(f2);
-        Txt_frase3.setText(f3);
-
+                for(int j=i; j<textos.length;j+=2){
+                    Txt_frase1.setText(textos[j]);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void showToolbar(String title, boolean upButton){
