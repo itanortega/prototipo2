96095eef55b96ff1c1140885858bd0de84754552
Christian Ortega <csortega@iucesmag.edu.co>
2018-05-24 09:16:23 -0500

    actualización de archivos

--- astross/astross/guardar.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $nombre = "archivos/".$_POST['nombre'].".json";
-    $contenido = $_POST['contenido'];
-    $fp = fopen($nombre,"w");
-    fwrite($fp, $contenido);
-    fclose($fp);
-    header('Location: ./archivos.php');
-
-?>
